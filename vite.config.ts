9490841fb647f370f84fc17a835ccce6b884ca0d
JORGE SOBRINHO <roteiro.ds@gmail.com>
2025-11-19 08:12:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// Vite + React
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      // tudo que começa com /api vai para o backend Node (server.js)
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
